2f9e980262af494b62ea715c44912221db1ee0b6
elianrichard <elianrichards@gmail.com>
2024-04-02 20:13:15 +0700

    Initial Commit

---
+++ Blip/BlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlipApp.swift
+//  Blip
+//
+//  Created by Elian Richard on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
